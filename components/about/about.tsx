f757c5296faf5bb3af7ea5fc596c8c7b2a13ffda
Qiyuan Chen <qiyuan.chen2002@gmail.com>
2024-01-15 02:49:02 +0000

    Added new skill and changed age

--- components/about/about.tsx
+++ components/about/about.tsx
@@ -7,11 +7,9 @@ const About: React.FC = () => {
 		<section className="about" id="about">
 			<h3>About me</h3>
 			<p>
-				I&apos;m Qiyuan Chen, a 19 year old student at Oxford
-				University studying Maths and Computer Science. When I
-				have some free time, I love using new web technologies to
-				develop cool web applications. Through this, I have developed a
-				variety of skills, including:
+				I&apos;m Qiyuan Chen, a 21 year old student at Oxford University studying Maths and Computer Science.
+				When I have some free time, I love using new web technologies to develop cool web applications. Through
+				this, I have developed a variety of skills, including:
 			</p>
 			<Icons />
 		</section>
